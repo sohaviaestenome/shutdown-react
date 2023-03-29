0c26d4ccba393de70225c16f993a4c84a791c4e2
pacs <pacs@outlook.pt>
2023-03-29 15:54:01 +0100

    packaging successfully

--- public/preload.js
+++ public/preload.js
@@ -1,10 +1,13 @@
-// public/preload.js
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electron", {
   send: (channel, data) => {
     ipcRenderer.send(channel, data);
   },
+  receive: (channel, callback) => {
+    ipcRenderer.on(channel, (event, ...args) => callback(...args));
+  },
+  remove: (channel, callback) => {
+    ipcRenderer.removeListener(channel, (event, ...args) => callback(...args));
+  },
 });
-
-console.log("Preload script loaded");
--- src/Components/TimeInputForm.js
+++ src/Components/TimeInputForm.js
@@ -1,11 +1,32 @@
 // src/Components/TimeInputForm.js
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TimeInput from "./TimeInput";
 import TimeToggle from "./TimeToggle";
 
 const TimeInputForm = () => {
   const [timeUnit, setTimeUnit] = useState("minutes");
   const [timeValue, setTimeValue] = useState("");
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const handleMessage = (event, msg) => {
+      setMessage(msg);
+    };
+  
+    window.electron.receive("shutdown-schedule-error", handleMessage);
+    window.electron.receive("shutdown-schedule-stderr", handleMessage);
+    window.electron.receive("shutdown-cancel-error", handleMessage);
+    window.electron.receive("shutdown-cancel-stderr", handleMessage);
+    window.electron.receive("shutdown-cancel-success", handleMessage);
+  
+    return () => {
+      window.electron.remove("shutdown-schedule-error", handleMessage);
+      window.electron.remove("shutdown-schedule-stderr", handleMessage);
+      window.electron.remove("shutdown-cancel-error", handleMessage);
+      window.electron.remove("shutdown-cancel-stderr", handleMessage);
+      window.electron.remove("shutdown-cancel-success", handleMessage);
+    };
+  }, []);
 
   const handleToggle = () => {
     setTimeUnit(timeUnit === "minutes" ? "hours" : "minutes");
@@ -22,12 +43,15 @@ const TimeInputForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <TimeInput timeValue={timeValue} setTimeValue={setTimeValue} />
-      <TimeToggle timeUnit={timeUnit} handleToggle={handleToggle} />
-      <button type="submit">Send Command</button>
-      <button onClick={handleCancelShutdown}>Cancel Shutdown</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <TimeInput timeValue={timeValue} setTimeValue={setTimeValue} />
+        <TimeToggle timeUnit={timeUnit} handleToggle={handleToggle} />
+        <button type="submit">Send Command</button>
+        <button onClick={handleCancelShutdown}>Cancel Shutdown</button>
+      </form>
+      <p>{message}</p>
+    </div>
   );
 };
 
